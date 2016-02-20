a44990f9cc6f068e0da1c21cf09cb8edc79d9092
Michelle Lin <mj.lin@berkeley.edu>
2016-02-19 20:02:46 -0800

    Replace lcs implementation with greedy
    Finding the longest common subsequence between each pair of
    subsequences and storing the results doesn't work because the
    table is not useful after merging subsequences.

--- Hard/254/dna_shotgun.py
+++ Hard/254/dna_shotgun.py
@@ -1,9 +1,6 @@
 import sys
 
-verbose = True
-
-input_seqs = []
-lcs_table = {}
+verbose = False
 
 def read_input_seqs(input_fname):
     """Returns a list of input DNA sequences read from filename INPUT_FNAME."""
@@ -11,76 +8,92 @@ def read_input_seqs(input_fname):
     input_seqs = [ line[:-1] for line in f ] # Strip \n
 
     if verbose:
-        print "> Reading input sequences from file '%s'." % input_fname
-        print "> ", input_seqs
-        print ""
+        print "> Reading input sequences from file '%s':" % input_fname
+        print ">", input_seqs
+
+    # Sort by string length
+    input_seqs.sort(key = lambda s : len(s))
 
     return input_seqs
 
-def populate_lcs():
-    """Populates a table of longest overlapping subsequences
-    between two strings.
-    """
-    table = {}
+def remove_substrings(input_seqs):
+    """Remove subsequences that are wholly contained in another subsequence."""
+    numSeqs = len(input_seqs)
+    input_list = input_seqs[:]
 
-    input_seqs.sort(key = lambda s: len(s))
+    for i in range(numSeqs):
+        for j in range(i + 1, numSeqs):
+            if input_seqs[i] in input_seqs[j]:
+                if verbose:
+                    print "> Removing '%s' which is contained in '%s'." \
+                          % (input_seqs[i], input_seqs[j])
+                input_list.remove(input_seqs[i])
+
+    return input_list
+
+def find_scs(input_seqs):
+    """Returns the shortest common supersequence composed of the subsequences
+    in INPUT_SEQS.
+    """
+    # Sort by string length, descending
     input_seqs.reverse()
 
+    # Start with longest subsequence
+    scs = input_seqs[0]
+    input_seqs.remove(scs)
+
+    while len(input_seqs) > 0:
+        longest_overlap = ""
+        next_to_merge = ""
+
+        for seq in input_seqs:
+
+            if verbose:
+                print "\n> Current shortest common supersequence:", scs
+                print "> Comparing with:", seq
+
+            for i in range(len(seq)):
+                for j in range(len(scs)):
+
+                    if seq[i] != scs[j]:
+                        continue
+
+                    overlap = ""
+                    length = 0
+
+                    while seq[i + length] == scs[j + length]:
+                        overlap += seq[i + length]
+                        length += 1
+
+                        if (i + length) >= len(seq) or \
+                           (j + length) >= len(scs): break
+
+                    if verbose:
+                        print "> Overlap is:", overlap
+
+                    if len(overlap) > len(longest_overlap):
+                        longest_overlap = overlap
+                        next_to_merge = seq
+
+                        if verbose:
+                            print "> Longest overlap is now:", longest_overlap
+
+        scs = merge_seqs(scs, next_to_merge, longest_overlap)
+        input_seqs.remove(next_to_merge)
+
+    return scs
+
+def merge_seqs(seqA, seqB, common):
+    """Merge subsequences SEQA and SEQB together, where len(seqA) >= len(seqB),
+    and the two strings have COMMON as their longest common subsequence.
+    """
     if verbose:
-        print "> Sorted input list by length, descending:"
-        print "> ", input_seqs
-        print ""
+        print "\n> Merging '%s' and '%s', which have '%s' in common." \
+              % (seqA, seqB, common)
 
-    for i in range(len(input_seqs)):
-        seqA = input_seqs[i]
-        table[seqA] = {}
+    overlap_start_idx = seqA.find(common)
+    return seqA[:overlap_start_idx] + seqB + seqA[overlap_start_idx + len(common):]
 
-        for j in range(i, len(input_seqs)):
-            seqB = input_seqs[j]
-            table[seqA][seqB] = find_overlap(seqA, seqB)
-
-    return table
-
-def find_overlap(a, b):
-    """Returns the longest overlapping substring between strings A and B."""
-    if len(a) > len(b):
-        longer = a
-        shorter = b
-    else:
-        longer = b
-        shorter = a
-
-    longest_overlap = ""
-    for i in range(len(shorter)):
-        for j in range(len(longer)):
-
-            if shorter[i] != longer[j]:
-                continue
-
-            overlap = ""
-            length = 0
-
-            while shorter[i + length] == longer[j + length]:
-                overlap += shorter[i + length]
-                length += 1
-                if i + length >= len(shorter) or j + length >= len(longer):
-                    break
-
-            if len(overlap) > len(longest_overlap): longest_overlap = overlap
-
-    return longest_overlap
-
-def merge_seqs(seqA, seqB):
-    if len(seqA) < len(seqB):
-        a = seqB
-        b = seqA
-    else:
-        a = seqA
-        b = seqB
-
-    common = lcs_table[a][b]
-    overlap_start_idx = a.find(common)
-    return a[:overlap_start_idx] + b + a[overlap_start_idx + len(common):]
-
-# input_seqs = read_input_seqs(sys.argv[1])
-# lcs_table = populate_lcs()
+fname = sys.argv[1]
+input_seqs = remove_substrings(read_input_seqs(fname))
+print(find_scs(input_seqs))
